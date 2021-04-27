405b178be76fa31163e69b4bc978f7254ff30400
Equinet Paul <45755573+EquinetPaul@users.noreply.github.com>
2021-04-27 23:35:01 +0200

    Update promoBar.php

--- dynamic/elements/promoBar.php
+++ dynamic/elements/promoBar.php
@@ -1,7 +1,10 @@
-<div class="promoBar pointer-cursor" id="promoBar">
+<php echo'
+     <div class="promoBar pointer-cursor" id="promoBar">
    <p>
       PROMO: 50% de réduction sur une séléction de produits!
       <strong>👉<a href="#" class="p-2 text-blue">C'est par ici</a></strong>
       <i class="fas fa-times hover-cursor-pointer" onclick="hideThisParent(this)"></i>
    </p>
 </div>
+';
+?>
